8dc2dc338ba0c1458498151277f4308f4014e781
Ana Letícia <analeticia.chavi@gmail.com>
2014-06-10 18:14:21 -0300

    Tentando de novo

---
+++ app/src/main/java/mobilecare/app/MainActivity.java
@@ -0,0 +1,47 @@
+package mobilecare.app;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.os.Environment;
+
+
+public class MainActivity extends Activity {
+
+    //Variáveis do Banco
+
+    private String CAMINHO = Environment.getExternalStorageDirectory().getAbsolutePath();
+    private String BANCO = "mobilecareBD.db";
+    public static SQLiteDatabase database;
+
+    private Intent menuPrincipal;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Criando o banco de dados se ele ainda não existir
+        System.out.println(CAMINHO + "/" + BANCO);
+        database = this.openOrCreateDatabase(CAMINHO + "/" + BANCO, 0, null);
+
+        //Criando as tabelas se elas ainda não existirem
+        database.execSQL("create table if not exists Medicamento(codMed integer primary key autoincrement, nome text not null, dosagem real not null, status integer not null, dataIni text not null, dataFim text, tarja text not null, obs text);");
+        database.execSQL("create table if not exists Horario(codHor integer primary key autoincrement, titulo text not null, data text not null, hora text not null, periodo integer not null,codMed integer not null, foreign key(codMed) references Medicamento(codMed));");
+        database.execSQL("create table if not exists Estoque(codEst integer primary key autoincrement, dataCompra text not null, qtdCartela integer not null, dataValidade text not null,codMed integer not null,codHor integer not null, foreign key(codMed) references Medicamento(codMed), foreign key(codHor) references Horario(codHor));");
+
+        menuPrincipal = new Intent(this, MenuPrincipal.class);
+
+
+
+        startActivity(menuPrincipal);
+
+    }
+
+
+}
+
+
+
+
---
+++ app/src/main/java/mobilecare/app/MenuMedicamento.java
@@ -0,0 +1,57 @@
+package mobilecare.app;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+
+
+public class MenuMedicamento extends Activity implements OnClickListener   {
+
+    private Button btnCadMedicamentoBD;
+    private Button btnPesqMedicamentoBD;
+    private Button btnRemMedicamentoBD;
+    private Button btnAtMedicamentoBD;
+
+    private Intent mudarTelaCadastroMed;
+    private Intent mudarTelaPesquisaMed;
+    private Intent mudarTelaRemoverMed;
+    private Intent mudarTelaAlterarMed;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_medicamento);
+
+        btnCadMedicamentoBD = (Button)findViewById(R.id.btnCadastrarMedicamento);
+        btnCadMedicamentoBD.setOnClickListener(this);
+        btnPesqMedicamentoBD = (Button)findViewById(R.id.btnPesquisarMedicamento);
+        btnPesqMedicamentoBD.setOnClickListener(this);
+        btnRemMedicamentoBD = (Button)findViewById(R.id.btnRemoverMedicamento);
+        btnRemMedicamentoBD.setOnClickListener(this);
+        btnAtMedicamentoBD = (Button)findViewById(R.id.btnAtualizarMedicamento);
+        btnAtMedicamentoBD.setOnClickListener(this);
+
+
+    }
+
+
+    @Override
+    public void onClick(View arg0) {
+        if(arg0.getId() == btnCadMedicamentoBD.getId()){
+            startActivity(mudarTelaCadastroMed);
+        }
+        else if(arg0.getId() == btnPesqMedicamentoBD.getId()){
+            startActivity(mudarTelaPesquisaMed);
+        }
+        else if(arg0.getId() == btnRemMedicamentoBD.getId()){
+            startActivity(mudarTelaRemoverMed);
+        }
+        else if(arg0.getId() == btnAtMedicamentoBD.getId()){
+            startActivity(mudarTelaAlterarMed);
+        }
+    }
+}
